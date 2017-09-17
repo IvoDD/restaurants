6f561cb5bcf1565408228a8618cda299de6f6dd9
daninnm <daninnm@abv.bg>
2017-09-17 22:35:00 +0300

    Started work on client

--- get_handler.js
+++ get_handler.js
@@ -11,6 +11,8 @@ exports.handle = function(express, app){
     app.use(bodyParser.json());
     
     app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-    app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+    app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS bootstrap-treeview
+    app.use('/js', express.static(__dirname + '/node_modules/bootstrap-treeview/dist')); // redirect JS jQuery
     app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+    app.use('/css', express.static(__dirname + '/node_modules/bootstrap-treeview/dist')); // redirect CSS bootstrap-treeview
 }
